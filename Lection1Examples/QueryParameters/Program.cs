eca4894de0503ddfc3454b1c7c36d13d357d7f0e
hlib602 <47103091+hlib602@users.noreply.github.com>
2022-09-09 13:24:06 +0300

    Add files via upload

---
+++ Lection1Examples/QueryParameters/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    var stringBuilder = new System.Text.StringBuilder("<h3>Параметри рядка запиту</h3><table>");
+    stringBuilder.Append("<tr><td>Параметр</td><td>Значення</td></tr>");
+    foreach (var param in context.Request.Query)
+    {
+        stringBuilder.Append($"<tr><td>{param.Key}</td><td>{param.Value}</td></tr>");
+    }
+
+    stringBuilder.Append("</table>");
+
+    await context.Response.WriteAsync(stringBuilder.ToString());
+});
+
+app.Run();
+
